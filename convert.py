58ba816e69952b7e22ba278c5073bff03be1f84e
sandes93 <sandeshpathak0112@gmail.com>
2022-08-14 12:30:50 +0930

    pything single folder working with subfolder code

--- convert.py
+++ convert.py
@@ -1,20 +1,31 @@
 import sys
 import os, stat
 from docx2pdf import convert
+from pathlib import Path
 
-NO_OTHER_WRITING = ''
 
 path = os.path.dirname(__file__)
 inputPath = os.path.join(path, 'test')
 print("inputpath", inputPath)
 
-for file in os.listdir(inputPath):
-	filename = os.fsdecode(file)
-	print(filename);
-	if filename.endswith('.docx'):
-		src = os.path.join(inputPath, filename)
-		print(src);
-		os.chmod(src, int('0777'))
-		outputPath = os.path.join(path, f'{filename}.pdf')
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+			# os.chmod(Path(file), int('0777'))
+            r.append(os.path.join(root, name))
+    return r
+
+files = list_files(inputPath);
+print(files)
+
+for file in files:
+	# filename = os.fsdecode(file)
+	# print(filename);
+	if file.endswith('.docx'):
+		# src = os.path.join(inputPath, filename)
+		print(file);
+		os.chmod(os.path.normpath(file), int('0777'))
+		outputPath = os.path.join(path, f'{file}.pdf')
 		print(outputPath)
-		convert(src,outputPath)
+		convert(file,outputPath)
