e1001065b27697b9dadae3382abbf9c88e20a4d0
sandes93 <sandeshpathak0112@gmail.com>
2022-08-14 12:10:57 +0930

    python single folder

--- convert.py
+++ convert.py
@@ -1,16 +1,20 @@
 import sys
-import os
+import os, stat
 from docx2pdf import convert
+
+NO_OTHER_WRITING = ''
 
 path = os.path.dirname(__file__)
 inputPath = os.path.join(path, 'test')
+print("inputpath", inputPath)
 
 for file in os.listdir(inputPath):
 	filename = os.fsdecode(file)
 	print(filename);
 	if filename.endswith('.docx'):
-		inputPath = os.path.join(path, filename)
+		src = os.path.join(inputPath, filename)
+		print(src);
+		os.chmod(src, int('0777'))
 		outputPath = os.path.join(path, f'{filename}.pdf')
-		print(inputPath)
 		print(outputPath)
-		convert(inputPath,outputPath)
+		convert(src,outputPath)
